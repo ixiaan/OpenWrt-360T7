503f23353f493381d90b8df785d2666d24682545
Selina <36494906+ixiaan@users.noreply.github.com>
2024-01-16 18:05:46 +0800

    Update diy3.sh

--- diy3.sh
+++ diy3.sh
@@ -21,5 +21,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.40.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='ImmortalWrt'/hostname='Buttfly'/g" package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By AnnShine@Yudie $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
