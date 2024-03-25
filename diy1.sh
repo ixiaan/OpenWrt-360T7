570a9e500916b8a39e2d9eac88d305c4a5b51a7d
Selina <36494906+ixiaan@users.noreply.github.com>
2024-03-26 01:37:21 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -14,13 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
-#sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
-#git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-#git clone https://github.com/Zxilly/UA2F package/UA2F
+sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+git clone https://github.com/Zxilly/UA2F package/UA2F
 # theme
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # rm -rf package/luci-theme-argon/README* package/luci-theme-argon/Screenshots/
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='ImmortalWrt'/hostname='360T7jiashan'/g" package/base-files/files/bin/config_generate
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By AnnShine $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
